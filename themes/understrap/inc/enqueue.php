a80bcfba1d0861aec89d3c355d88d1030d403a5b
JoshuaGale <joshua.gale@my.jcu.edu.au>
2019-05-22 14:49:39 +1000

    Attempt bug fix

--- themes/understrap/inc/enqueue.php
+++ themes/understrap/inc/enqueue.php
@@ -18,7 +18,7 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 		$the_theme     = wp_get_theme();
 		$theme_version = $the_theme->get( 'Version' );
 
-		$css_version = $theme_version . '.' . filemtime( get_template_directory() . '/css/theme.min.css' );
+		$css_version = $theme_version . '.' . filemtime( get_template_directory() . '/css/theme.css' );
 		wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/css/theme.css', array(), $css_version );
 
 		wp_enqueue_script( 'jquery' );
